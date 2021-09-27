f069c81020c954c851f0783ce7bd412afffeb445
minna <minna.nguyen@tuni.fi>
2021-09-27 18:46:08 +0300

    e11 muokattu hieman

--- homework/2021-38/e11/Main.java
+++ homework/2021-38/e11/Main.java
@@ -3,7 +3,7 @@
 class Main{
     public static void main (String [] args){
 
-      double random = Math.random()*9;
+      double random = Math.random()*10;
         int i = (int)random;
         System.out.println(i);        
   }
